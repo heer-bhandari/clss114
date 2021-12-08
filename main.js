2c6710a0dec4075b928523a54e3840253085f2e9
heer-bhandari <75120883+heer-bhandari@users.noreply.github.com>
2021-12-08 17:59:42 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+function preload(){
+}
+function setup (){
+    canvas = createCanvas(300,300);
+    canvas.center();
+}
+function draw(){
+}
+function take_pic(){
+    save("My_filter_pic.png");
+}
